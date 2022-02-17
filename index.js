dad2e25805afe96e2feac65df553e667b57e6009
Bexter <bongk3164@gmail.com>
2022-02-17 21:24:24 +0900

    web3 연습

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const Web3 = require("web3");
+
+function getWeb3() {
+  const web3 = new Web3(
+    new Web3.providers.HttpProvider("http://127.0.0.1:7545")
+  );
+  return web3;
+}
+
+async function getAccounts() {
+  try {
+    const accounts = await getWeb3().eth.getAccounts();
+    console.log(accounts);
+    return accounts;
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
+}
+
+async function getGasPrice() {
+  try {
+    const gasPrice = await getWeb3().eth.getGasPrice();
+    console.log(gasPrice);
+    return gasPrice;
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
+}
+
+async function getBlock() {
+  try {
+    const getBlock = await getWeb3().eth.getBlock("latest");
+    console.log(getBlock);
+    return getBlock;
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
+}
+
+app.get("/getblock", (req, res) => {
+  getBlock().then((getBlock) => {
+    res.send(getBlock);
+  });
+});
+
+app.get("/gasprice", (req, res) => {
+  getGasPrice().then((gasPrice) => {
+    res.send(gasPrice);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello Node.js!");
+});
+
+app.listen(port, () => {
+  console.log("Listening...");
+});
